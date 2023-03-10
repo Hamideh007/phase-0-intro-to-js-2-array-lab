1efabbac69d443a80f40b009158322b74ba3d148
hamideh rahmani <rahmani.mahtab@gmail.com>
2023-03-10 00:46:26 -0500

    complete arry-lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name){
+    cats.push("Ralph");
+}
+function destructivelyPrependCat(name){
+    cats.unshift("Bob");
+}
+function destructivelyRemoveLastCat(name){
+    cats.pop();
+}
+function destructivelyRemoveFirstCat(name){
+    cats.shift();
+}
+function appendCat(name){
+    return [...cats, name]
+}
+beforeEach();
+function prependCat(name){
+    return [name, ...cats];
+}
+beforeEach();
+function removeLastCat(name){
+return cats.slice(0, length- 1 );
+}
+beforeEach();
+function removeFirstCat(name){
+    return cats.slice(1)
+}
+beforeEach(); 
